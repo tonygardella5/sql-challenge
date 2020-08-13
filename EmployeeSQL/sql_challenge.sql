2621518778112da690a6d568c57910265342bef6
unknown <tony.gardella5@gmail.com>
2020-08-12 23:59:33 -0500

    a couple more steps completed

--- EmployeeSQL/sql_challenge.sql
+++ EmployeeSQL/sql_challenge.sql
@@ -1,20 +1,17 @@
 create table departments (
 dept_no varchar not null,
 dept_name varchar not null,
-primary key (dept_no)
-);
+primary key (dept_no));
 
 create table dept_emp (
 emp_no int not null,
 dept_no varchar not null,
-primary key (emp_no, dept_no)
-);
+primary key (emp_no, dept_no));
 
 create table dept_manager (
 dept_no varchar not null,
 emp_no int not null,
-primary key (dept_no, emp_no)
-);
+primary key (dept_no, emp_no));
 
 create table employees (
 emp_no int not null,
@@ -24,26 +21,22 @@ first_name varchar not null,
 last_name varchar not null,
 sex varchar not null,
 hire_date date not null,
-primary key (emp_no)
-);
+primary key (emp_no));
 
 create table salaries (
 emp_no int not null,
 salary int not null,
-primary key (emp_no)
-);
+primary key (emp_no));
 
 create table titles (
 title_id varchar not null,
 title varchar not null,
-primary key (title_id)
-);
+primary key (title_id));
 
 select employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 from employees
 left join salaries
-on (employees.emp_no = salaries.emp_no)
-;
+on (employees.emp_no = salaries.emp_no);
 
 
 select hire_date from employees;
