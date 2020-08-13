1664792237c1d45a2570cf3a6d0b5d6516faa59b
unknown <tony.gardella5@gmail.com>
2020-08-12 23:32:04 -0500

    a couple more steps complete

--- EmployeeSQL/sql_challenge.sql
+++ EmployeeSQL/sql_challenge.sql
@@ -5,31 +5,31 @@ primary key (dept_no)
 );
 
 create table dept_emp (
-emp_no INT not null,
+emp_no int not null,
 dept_no varchar not null,
 primary key (emp_no, dept_no)
 );
 
 create table dept_manager (
 dept_no varchar not null,
-emp_no INT not null,
+emp_no int not null,
 primary key (dept_no, emp_no)
 );
 
 create table employees (
-emp_no INT not null,
+emp_no int not null,
 emp_title_id varchar not null,
-birth_date DATE not null,
+birth_date date not null,
 first_name varchar not null,
 last_name varchar not null,
 sex varchar not null,
-hire_date DATE not null,
+hire_date date not null,
 primary key (emp_no)
 );
 
 create table salaries (
-emp_no INT not null,
-salary INT not null,
+emp_no int not null,
+salary int not null,
 primary key (emp_no)
 );
 
@@ -49,5 +49,23 @@ select hire_date from employees;
 
 select first_name, last_name, hire_date
 from employees
-WHERE (hire_date >= '1986-01-01' AND 
+where (hire_date >= '1986-01-01' AND 
        hire_date <= '1986-12-31'); 
+
+select dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, 
+	employees.first_name
+from dept_manager
+inner join departments
+	on (dept_manager.dept_no = departments.dept_no)
+inner join employees
+	on (dept_manager.emp_no = employees.emp_no);
+	
+	
+select last_name, first_name
+from employees
+where emp_no in
+	(select emp_no
+	from dept_emp
+	where dept_name in (
+		select dept_name
+		from departments));
